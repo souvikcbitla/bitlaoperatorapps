a1d70660b2a2a73377056551fc3fcf02445ff35c
souvikchaudhury <souvik.c@bitlasoft.com>
2024-12-11 10:20:41 +0530

    download script page modified

--- register.js
+++ register.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebase/10.7.2/firebase-auth.js'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
